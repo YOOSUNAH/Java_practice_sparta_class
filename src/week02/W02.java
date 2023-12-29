377274e7c6a7969547eb4b29863ee8ab26463bf4
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-12-29 19:33:28 +0900

    사칙연산 학습

---
+++ src/week02/W02.java
@@ -0,0 +1,47 @@
+package week02;
+
+public class W02 {
+    public static void main(String[] args) {
+        // 사칙 연산 : +, -, *, /, %
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2
+        System.out.println(5 / 2); // 2 몫만 줌
+        System.out.println(2 / 4); // 0
+        System.out.println(4 % 2); // 0 나머지
+        System.out.println(5 % 2); // 1 나머지
+        System.out.println("----------------");
+
+        // 우선 순위 연산
+        System.out.println(2 + 2 * 2); // 6 곱하기 먼저 계산
+        System.out.println((2 + 2) * 2); // 8
+        System.out.println(2 + (2 * 2)); // 6 (첫번째와 동일)
+
+        // 변수를 이용한 연산
+        int a = 20;
+        int b = 10;
+        int c;
+
+        // 덧셈
+        c = a + b;
+        System.out.println(c); // 30
+        c = a - b;
+        System.out.println(c); // 10
+
+        // 곱셈
+        c = a * b;
+        System.out.println(c); // 200
+
+        // 나눗셈(몫)
+        c = a / b;
+        System.out.println(c); // 2
+
+        // 나눗셈(나머지)
+        c = a % b;
+        System.out.println(c); // 0
+
+
+
+    }
+}
