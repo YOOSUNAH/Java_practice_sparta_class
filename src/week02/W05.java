17df9b1620591182713040c4b8bb64f4360eded0
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-12-29 20:00:10 +0900

    대입연산자 학습
    대입연산자에서 주의해야할 점 포함

---
+++ src/week02/W05.java
@@ -0,0 +1,60 @@
+package week02;
+
+public class W05 {
+    // 대입 연산자
+    // 대입 연산자 : =, ++, --
+    public static void main(String[] args) {
+        // 변수를 바로 연산해서 그 자리에서 저장하는(대입하는) 연산자
+        // = (기본 연산자), +=, -=, *= ....(복합대입연산자)
+        // ++ : += 1
+        // -- : -= 1
+
+        int number = 10;
+        number = number + 2;
+        System.out.println(number); // 12
+
+        number = number - 2;
+        System.out.println(number); // 10
+
+        number = number * 2;
+        System.out.println(number); // 20
+
+        number = number / 2;
+        System.out.println(number); // 10
+
+        number = number % 2;
+        System.out.println(number); // 0
+
+        System.out.println("------------");
+
+        // 복합 대입 연산자
+        number = 10;
+
+        number += 2;  // number = number + 2; 를 줄여서 표현한 것
+        System.out.println(number);  //12
+
+        number -= 2;  // number = number - 2;
+        System.out.println(number);  //10
+
+        number *= 2;  // number = number * 2;
+        System.out.println(number);  //20
+
+        number /= 2;  // number = number / 2;
+        System.out.println(number);  // 10
+
+        number %= 2;  // number = number % 2;
+        System.out.println(number);  // 0
+
+        System.out.println("------------");
+
+        // ++, --
+        number = number + 1; // 이걸 줄여서 표현한 것이 아래
+        number += 1; // 이걸 줄여서 표현한 것이 아래
+        number++;
+        System.out.println(number);  // 3
+
+        number--;
+        System.out.println(number); // 2
+
+    }
+}
---
+++ src/week02/W06.java
@@ -0,0 +1,20 @@
+package week02;
+
+public class W06 {
+    public static void main(String[] args) {
+        // 대입 연산자에서 주의 해야 할 점!!
+        // ++, -- 를 사용할 때
+        int a = 10;
+        int b = 10;
+        int val = ++a + b--;  // 11 + 9 = 20 예상하지만 아님.
+        System.out.println(a); // 11
+        System.out.println(b); // 9
+        System.out.println(val); // 21
+        // ++, -- 앞에 있으면  ++가 붙은 값을 대체한 다음에 한줄이 실행이 되고 다음줄로 넘어간다.
+        // a 에 +1을 한 값을 대입하고 한줄을 실행한 다음 b에 -1을 해줘 가 된다.
+
+        val = ++a + --b; // 이렇게해야 11 + 9 = 20 와 같이 계산됨
+        System.out.println(val);
+
+    }
+}
